46668948aa37751153ecd1cc9ce8ca4b09765772
Zhuowei Zhang <zhuoweiz10@gmail.com>
2022-12-02 01:04:02 -0500

    minor naming improvement

--- AI_Monopoly_Games/BoardCell.py
+++ AI_Monopoly_Games/BoardCell.py
@@ -1,3 +1,5 @@
+import random
+
 class BoardCell:
 
    def __init__(self, index):
@@ -7,13 +9,13 @@ class BoardCell:
 
       self.player = [] # list of index of players on this cell
 
-      self.land_cost = 50 # player pays this much to own the cell
-      self.rent_cost = 5 # player pays this much to other player if the cell is owned
+      self.land_price = random.randint(50,99) # player pays this much to own the cell
+      self.land_rent = random.randint(5,20) # player pays this much to other player if the cell is owned
 
       self.owner = None # player who owns the cell, 0 or 1
 
    def print_info(self):
-      print("BoardCell: index = {}, has_lucky_box = {}, lucky_cash_amount = {}, land_cost = {}, rent_cost = {}".format(self.index, self.has_lucky_box, self.lucky_cash_amount, self.land_cost, self.rent_cost))
+      print("BoardCell: index = {}, has_lucky_box = {}, lucky_cash_amount = {}, land_price = {}, land_rent = {}".format(self.index, self.has_lucky_box, self.lucky_cash_amount, self.land_price, self.land_rent))
    
    def pad_number(self, number:int) -> str:
       return str(number).rjust(2)
@@ -23,15 +25,15 @@ class BoardCell:
       if self.owner is not None:
          result += "O{},".format(self.pad_number(self.owner))
       else :
-         result += "P{},".format(self.pad_number(self.land_cost))
-      result += "R{},".format(self.pad_number(self.rent_cost))
+         result += "P{},".format(self.pad_number(self.land_price))
+      result += "R{},".format(self.pad_number(self.land_rent))
       if self.has_lucky_box:
          result += "L{}".format(self.pad_number(self.lucky_cash_amount))
 
       # pad
       result = "[" + str(self.index).zfill(2) + " " + result.ljust(11) +  " | "
 
-      # drop players
+      # display players
       player_string = ""
       for player_index in self.player:
          player_string += str(player_index) + " "
