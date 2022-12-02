6e93c31ac18bfcb53096a69b5ae4cf661c097111
Zhuowei Zhang <zhuoweiz10@gmail.com>
2022-12-02 02:53:53 -0500

    improved user prompt experience

--- AI_Monopoly_Games/play_monoply.py
+++ AI_Monopoly_Games/play_monoply.py
@@ -12,10 +12,12 @@ if __name__ == "__main__":
 	while (True):
 		prompt = "Please choose the size of the game board: A: 16, B: 18, C: 20, D: 22, E: 24: \n"
 		game_size_choice = input(prompt)
-		if (ord(game_size_choice) - ord('A') < 0 or ord(game_size_choice) - ord('A') > 4):
+		choice_int = min(ord(game_size_choice) - ord('A'), ord(game_size_choice) - ord('a'))
+		if (choice_int < 0 or choice_int > 4):
 			print("Invalid input, please try again.")
 		else:
-			game_size = 20 + 2 * (ord(game_size_choice) - ord('A'))
+			game_size = 20 + 2 * choice_int
+			print("You have chosen a game board of size {}.".format(game_size))
 			game = Game(grid_size=game_size)
 			break
 	
@@ -23,20 +25,22 @@ if __name__ == "__main__":
 	while (True):
 		prompt = "Please choose player type for PLAYER ZERO: A: Human, B: Baseline AI: \n"
 		player_zero_choice = input(prompt)
-		if (ord(player_zero_choice) - ord('A') < 0 or ord(player_zero_choice) - ord('A') > 1):
+		choice_int = min(ord(player_zero_choice) - ord('A'), ord(player_zero_choice) - ord('a'))
+		if (choice_int < 0 or choice_int > 1):
 			print("Invalid input, please try again.")
 		else:
-			player_zero_type = ord(player_zero_choice) - ord('A')
+			player_zero_type = choice_int
 			game.set_player_type(0, player_zero_type)
 			break
 	
 	while (True):
 		prompt = "Please choose player type for PLAYER ONE: A: Human, B: Baseline AI: \n"
 		player_one_choice = input(prompt)
-		if (ord(player_one_choice) - ord('A') < 0 or ord(player_one_choice) - ord('A') > 1):
+		choice_int = min(ord(player_one_choice) - ord('A'), ord(player_one_choice) - ord('a'))
+		if (choice_int < 0 or choice_int > 1):
 			print("Invalid input, please try again.")
 		else:
-			player_one_type = ord(player_one_choice) - ord('A')
+			player_one_type = choice_int
 			game.set_player_type(1, player_one_type)
 			break
 
